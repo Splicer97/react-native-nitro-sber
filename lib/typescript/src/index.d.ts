726b95242922cd1970821cd68b529bf563eb7a8e
Nikita Kovantsov <kovancov@gmail.com>
2026-03-19 11:08:36 +0300

    feat: enhance SberModule with initialization and authentication functions
    This commit adds the `initialize` and `auth` functions to the SberModule, allowing for better integration with the NitroSber module. It also updates TypeScript definitions to include new props for these functions, ensuring type safety and improved developer experience. Source maps for the updated files are included for better debugging.

--- lib/typescript/src/index.d.ts
+++ lib/typescript/src/index.d.ts
@@ -1,4 +1,27 @@
 import type { NitroSber } from './NitroSber.nitro';
+import { SID_STAND } from './NitroSber.nitro';
 export declare const SberModule: NitroSber;
-export { SID_STAND } from './NitroSber.nitro';
+export { SID_STAND };
+export type InitializeProps = {
+    clientId: string;
+    partnerName?: string;
+    userID?: string;
+    themeColorLight?: string;
+    themeColorDark?: string;
+    partnerProfileUrl?: string;
+    isShowErrorOnMain?: boolean;
+    standType?: SID_STAND;
+};
+export declare function initialize(props: InitializeProps): Promise<void>;
+export type AuthProps = {
+    scope: string;
+    state: string;
+    nonce: string;
+    redirectUri: string;
+    ssoBaseUrl?: string;
+    codeChallenge?: string;
+    codeChallengeMethod?: string;
+    loginHint?: string;
+};
+export declare function auth(props: AuthProps): Promise<void>;
 //# sourceMappingURL=index.d.ts.map
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,56 @@
 import { NitroModules } from 'react-native-nitro-modules';
 import type { NitroSber } from './NitroSber.nitro';
+import { SID_STAND } from './NitroSber.nitro';
 
 export const SberModule =
   NitroModules.createHybridObject<NitroSber>('NitroSber');
 
-export { SID_STAND } from './NitroSber.nitro';
+export { SID_STAND };
+
+export type InitializeProps = {
+  clientId: string;
+  partnerName?: string;
+  userID?: string;
+  themeColorLight?: string;
+  themeColorDark?: string;
+  partnerProfileUrl?: string;
+  isShowErrorOnMain?: boolean;
+  standType?: SID_STAND;
+};
+
+export function initialize(props: InitializeProps): Promise<void> {
+  return SberModule.initialize(
+    props.clientId,
+    props.partnerName,
+    props.userID,
+    props.themeColorLight,
+    props.themeColorDark,
+    props.partnerProfileUrl,
+    props.isShowErrorOnMain,
+    props.standType
+  );
+}
+
+export type AuthProps = {
+  scope: string;
+  state: string;
+  nonce: string;
+  redirectUri: string;
+  ssoBaseUrl?: string;
+  codeChallenge?: string;
+  codeChallengeMethod?: string;
+  loginHint?: string;
+};
+
+export function auth(props: AuthProps): Promise<void> {
+  return SberModule.auth(
+    props.scope,
+    props.state,
+    props.nonce,
+    props.redirectUri,
+    props.ssoBaseUrl,
+    props.codeChallenge,
+    props.codeChallengeMethod,
+    props.loginHint
+  );
+}
